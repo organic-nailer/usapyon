d7af1210414b1b9c929325da5f43f15966531250
organic-nailer <hykwyuk0125@gmail.com>
2022-12-31 16:11:00 +0900

    fix share link

--- lib/logic/share_sns.dart
+++ lib/logic/share_sns.dart
@@ -3,7 +3,7 @@ import 'package:url_launcher/url_launcher_string.dart';
 
 const _shareUrl = "https://usapyon.fastriver.dev";
 
-const _twitterShareLink = """https://twitter.com/share?url=$_shareUrl&text=""";
+const _twitterShareLink = """https://twitter.com/intent/tweet?url=$_shareUrl&text=""";
 
 const _facebookShareLink = """http://www.facebook.com/share.php?u=$_shareUrl""";
 
