b629af8f093694f9ca7e395168e7a8d80dad1dfa
organic-nailer <hykwyuk0125@gmail.com>
2022-12-31 22:44:21 +0900

    update font

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:usapyon/page/game_page.dart';
 import 'package:usapyon/page/result_page.dart';
 import 'package:usapyon/page/sensor_page.dart';
@@ -18,6 +19,7 @@ class MyApp extends StatelessWidget {
         title: '帰ってきたうさぴょん',
         theme: ThemeData(
           primarySwatch: Colors.teal,
+          textTheme: GoogleFonts.kiwiMaruTextTheme()
         ),
     //    home: StagePreviewPage(),);
     //    home: const GamePage());
